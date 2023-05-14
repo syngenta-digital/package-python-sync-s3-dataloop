b1ea23c370cbc75ee6f071666191f9b4cafeca2f
Gabriel Sousa <gabriel.sousa@syngenta.com>
2023-05-14 19:39:45 -0300

    fix deleted files on previous commit

--- sync_s3_and_dataloop/__main__.py
+++ sync_s3_and_dataloop/__main__.py
@@ -6,9 +6,13 @@ def main():
     parser.add_argument('-ec2', action='store_true', help='Use this flag only if want to login on dataloop sdk using a bot account on ec2 instance')
     parser.add_argument('-images', action='store_true')
     parser.add_argument('-annotations', action='store_true')
+    parser.add_argument('-dl_email', required=True)
+    parser.add_argument('-dl_password', required=True)
+
+
     args = parser.parse_args()
 
-    dl_login(args.ec2)
+    dl_login(args.ec2, args.dl_email, args.dl_password)
 
     if args.images:
         sync_images_to_dataloop(project_settings)
--- sync_s3_and_dataloop/dataloop_login.py
+++ sync_s3_and_dataloop/dataloop_login.py
@@ -1,9 +1,9 @@
 import dtlpy as dl
 
-def dl_login(ec2_login_method):
+def dl_login(ec2_login_method, dl_email, dl_password):
     if dl.token_expired():
         if ec2_login_method:
-            dl.login_m2m(email= 'bot.722580db-11f0-4069-89e1-bc69e9e5fb1f@bot.dataloop.ai', password='b!eLQN7^&3N391n10')
+            dl.login_m2m(email= dl_email, password=dl_password)
         else:
             dl.login()
     
